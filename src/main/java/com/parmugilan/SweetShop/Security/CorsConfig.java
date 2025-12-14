bc0f7f3016baaff95feabbc5e96b5c72491b87a6
Parmugilan S <mukilan895@gmail.com>
2025-12-14 20:21:20 +0530

    d

--- src/main/java/com/parmugilan/SweetShop/Security/CorsConfig.java
+++ src/main/java/com/parmugilan/SweetShop/Security/CorsConfig.java
@@ -1,3 +1,10 @@
+package com.parmugilan.SweetShop.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
 
